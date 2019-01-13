149f3c88d8670898fc734ce8d2c86b87d4dc5542
Petter <petter@host-193-11-185-10.eduroam.bth.se>
2019-01-13 14:33:55 +0100

    gitignore

---
+++ Vector.hpp
@@ -0,0 +1,15 @@
+//
+// Created by patSurface on 2018-11-26.
+//
+
+#ifndef ASSIGNMENT_A2_PETTERJ123_VECTOR_H
+#define ASSIGNMENT_A2_PETTERJ123_VECTOR_H
+
+
+class Vector {
+public:
+    float x, y;
+};
+
+
+#endif //ASSIGNMENT_A2_PETTERJ123_VECTOR_H
